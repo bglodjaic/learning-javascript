1a4f9b98277180aa3841bb75e2a12e87b7814bfd
Bojan <glbojan@gmail.com>
2014-07-10 09:16:11 +0200

    removing yo test app

--- yo-test-app/app/scripts/core.js
+++
@@ -1,9 +0,0 @@
-/* global define */
-define( function () {
-    'use strict';
-
-    var module = { name:'core' };
-
-    console.log('Module: ' + module.name + ' operational.');
-    return module;
-});
--- yo-test-app/app/scripts/main.js
+++
@@ -1,7 +0,0 @@
-console.log('Application bootstrap');
-require(['core'], function(core){
-    'use strict';
-    /* This function will be called after module(s) has been executed */
-
-    console.log('module: \'core\' has been loaded: ', core);
-});
